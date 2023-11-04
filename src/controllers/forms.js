bfefa5b2e64dff0c7779181441effdf88d50d2ad
Ahmed Magdi <ammhaa2012@gmail.com>
2023-11-04 19:53:12 +0200

    Add getAll, and del forms controllers

--- src/controllers/forms.js
+++ src/controllers/forms.js
@@ -11,3 +11,41 @@ exports.addNewForm = async (req, res, next) => {
         next(err);
     }
 };
+
+exports.getAllForms = async (req, res, next) => {
+    try {
+        const by = req.auth.payload.sub;
+        const forms = await Form.find({ by }, "_id title description");
+        res.status(200).json(forms);
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.deleteForm = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const by = req.auth.payload.sub;
+        const form = await Form.findById(id);
+
+        // Form not found
+        if (!form) {
+            const err = new Error("Form not found");
+            err.status = 404;
+            throw err;
+        }
+
+        // Unauthorized
+        if (form.by !== by) {
+            const err = new Error("User not authorized to delete this form");
+            err.status = 403;
+            throw err;
+        }
+
+        // Delete the form
+        await Form.deleteOne({ _id: id });
+        res.status(200).json({ message: `${form.title} was deleted` });
+    } catch (error) {
+        next(error);
+    }
+};
--- src/routes/forms.js
+++ src/routes/forms.js
@@ -1,8 +1,13 @@
 const authUserMiddleware = require("../../middlewares/authUserMiddleware");
-const { addNewForm } = require("../controllers/forms");
+const { addNewForm, getAllForms, deleteForm } = require("../controllers/forms");
+const Form = require("../models/Form");
 
 const router = require("express").Router();
 
+router.get("/get/all", authUserMiddleware, getAllForms);
+
 router.post("/new", authUserMiddleware, addNewForm);
+
+router.delete("/del/:id", authUserMiddleware, deleteForm);
 
 module.exports = router;
