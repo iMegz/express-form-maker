f371d67015dd9649c9a67a1e70f2ee0d6f607393
Ahmed Magdi <ammhaa2012@gmail.com>
2023-11-15 00:40:50 +0200

    Fix missing controller and optimize form stats

--- src/routes/subscription.js
+++ src/routes/subscription.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const { subscribe } = require("../controllers/subscription");
 const authUserMiddleware = require("../middlewares/authUserMiddleware");
 
-router.post("/subscribe", authUserMiddleware);
+router.post("/subscribe", authUserMiddleware, subscribe);
 
 module.exports = router;
